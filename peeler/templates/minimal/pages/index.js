33d209277b7350735deccc6b43fc47159935beef
Marlon Marcello <marlon.marcello@gmail.com>
2021-04-01 11:06:51 -0700

    fix(peeler): templates erroring with missing variables

--- peeler/templates/minimal/pages/index.js
+++ peeler/templates/minimal/pages/index.js
@@ -3,7 +3,7 @@ import BarfyStars from "https://cdn.skypack.dev/wtc-barfystars";
 const barfTitle = new BarfyStars(document.querySelector("h1"));
 
 for (let i = 0; i < 3; i++) {
-  setInterval(() => {
+  setTimeout(() => {
     barfTitle.barf();
-  }, i * 1000);
+  }, i * 800);
 }
