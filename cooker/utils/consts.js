2bd6acd5095d6d3706ff89e95a64ecd8159999de
Andrew Rubin <arubin78@gmail.com>
2021-05-06 09:59:04 -0700

    update(image compression): swap CACHE_DIRECTORY for CWD

--- cooker/utils/consts.js
+++ cooker/utils/consts.js
@@ -36,7 +36,6 @@ if (fse.pathExistsSync(CONFIG_PATH)) {
 }
 
 const BUILD_DIRECTORY = path.join(CWD, CONFIG.buildDirectory);
-const CACHE_DIRECTORY = path.join(CWD, ".cache", "sweet-potato-cooker");
 
 const SOURCE_DIRECTORY = path.join(CWD, CONFIG.sourceDirectory);
 const PAGES_DIRECTORY = path.join(SOURCE_DIRECTORY, "pages");
@@ -52,7 +51,6 @@ module.exports = {
   CONFIG,
   ROOT_DIRECTORY,
   BUILD_DIRECTORY,
-  CACHE_DIRECTORY,
   SOURCE_DIRECTORY,
   PAGES_DIRECTORY,
   PUBLIC_DIRECTORY,
