8b98fc1f04add969a0d8fbb5323145c8a2d0e420
Liam Egan <liam@wethecollective.com>
2021-07-08 12:41:03 -0700

    Removing barfy stars from titles

--- documentation/pages/index.js
+++
@@ -1,9 +0,0 @@
-import BarfyStars from "https://cdn.skypack.dev/wtc-barfystars";
-
-const barfTitle = new BarfyStars(document.querySelector("h1"));
-
-for (let i = 0; i < 3; i++) {
-  setTimeout(() => {
-    barfTitle.barf();
-  }, i * 800);
-}
