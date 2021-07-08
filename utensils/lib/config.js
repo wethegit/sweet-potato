d484a94cf3840e54cd4a3d10f9405ee384474271
Andrew Rubin <arubin78@gmail.com>
2021-07-08 10:14:21 -0700

    fix(svgo): update options object to extend defaults

--- utensils/lib/config.js
+++ utensils/lib/config.js
@@ -1,3 +1,4 @@
+const { extendDefaultPlugins } = require("svgo");
 const path = require("path");
 const fse = require("fs-extra");
 
@@ -27,8 +28,7 @@ let CONFIG = {
     },
     imageminGifsicle: {},
     imageminSvgo: {
-      plugins: [{ name: "removeViewBox", active: false }],
-      multipass: true,
+      plugins: extendDefaultPlugins([{ name: "removeViewBox", active: false }]),
     },
   },
   breakpoints: false,
