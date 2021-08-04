ec95e70192d30ad37582e35c2539b11d1280365a
Liam Egan <liam@wethecollective.com>
2021-08-04 15:20:50 -0700

    Adding logger to exitWithError so that errors actually get logged

--- peeler/lib/exitWithError.js
+++ peeler/lib/exitWithError.js
@@ -1,7 +1,9 @@
+const { logger } = require("@wethegit/sweet-potato-utensils");
+
 function exitWithError(message, opts) {
   logger.error(message, opts);
   console.error("Cannot continue safely. Exiting...");
   process.exit(1);
 }
 
-module.export = exitWithError;
+module.exports = exitWithError;
